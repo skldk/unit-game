a58daf70ed6f0149e0c2d8b66bf15d3a0f232a32
skldk <skladko.ab@gmail.com>
2025-04-29 08:46:51 +0300

    Add application structure documentation

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,11 @@
+/**
+ * Структура приложения:
+ * 1. public/index.html содержит <div id="root">
+ * 2. src/index.tsx монтирует React в этот div
+ * 3. src/App.tsx определяет основной layout
+ * 4. src/pages/EconomySimulator.tsx содержит основную логику игры
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
